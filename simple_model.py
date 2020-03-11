9b2331283b7d70e7b4a58905cf1f1eecffa4ab5a
Shreerama <55805309+Shreerama9@users.noreply.github.com>
2020-03-11 19:43:39 +0530

    Update simple_model.py

--- simple_model.py
+++ simple_model.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn import datasets , linear_model
 from sklearn.metrics import mean_squared_error
 
+# a and b are random arrays created by me😅
+
 a = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9]])
 b = np.array([[3],[2],[4],[8],[7],[1],[9],[5],[6]])
 
